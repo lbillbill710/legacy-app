831f10c057c45e0855103b1be640f07a4e7a6706
lbillbill710 <lbillbill710@gmail.com>
2026-08-01 06:19:05 -0400

    Create weekly_review.dart

---
+++ lib/models/weekly_review.dart
@@ -0,0 +1,17 @@
+class WeeklyReview {
+  final String week;
+  final String biggestWin;
+  final String growthArea;
+  final String memoryCreated;
+  final String lessonTaught;
+  final String nextFocus;
+
+  WeeklyReview({
+    required this.week,
+    required this.biggestWin,
+    required this.growthArea,
+    required this.memoryCreated,
+    required this.lessonTaught,
+    required this.nextFocus,
+  });
+}
