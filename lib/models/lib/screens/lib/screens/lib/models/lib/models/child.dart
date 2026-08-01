f29f4f56679288140a7e8703c00cec91bb8e880c
lbillbill710 <lbillbill710@gmail.com>
2026-08-01 06:22:32 -0400

    Create child.dart

---
+++ lib/models/lib/screens/lib/screens/lib/models/lib/models/child.dart
@@ -0,0 +1,15 @@
+class Child {
+  final String name;
+  final DateTime birthday;
+  final List<String> interests;
+  final List<String> memories;
+  final List<String> lessonsLearned;
+
+  Child({
+    required this.name,
+    required this.birthday,
+    required this.interests,
+    required this.memories,
+    required this.lessonsLearned,
+  });
+}
