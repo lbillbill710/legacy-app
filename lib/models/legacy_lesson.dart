a46bf1684d0e188ad4f4a02459efdac7b9625a7c
lbillbill710 <lbillbill710@gmail.com>
2026-08-01 06:26:02 -0400

    Create legacy_lesson.dart

---
+++ lib/models/legacy_lesson.dart
@@ -0,0 +1,19 @@
+class LegacyLesson {
+  final String title;
+  final String topic;
+  final String description;
+  final String childName;
+  final String ageRange;
+  final String lessonLearned;
+  final DateTime dateCreated;
+
+  LegacyLesson({
+    required this.title,
+    required this.topic,
+    required this.description,
+    required this.childName,
+    required this.ageRange,
+    required this.lessonLearned,
+    required this.dateCreated,
+  });
+}
