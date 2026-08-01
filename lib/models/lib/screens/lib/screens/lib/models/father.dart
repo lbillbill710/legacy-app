ee8c54cd7cfd732dd70a7ad71b948423a185fa82
lbillbill710 <lbillbill710@gmail.com>
2026-08-01 06:21:16 -0400

    Create father.dart

---
+++ lib/models/lib/screens/lib/screens/lib/models/father.dart
@@ -0,0 +1,13 @@
+class Father {
+  final String name;
+  final String fatherhoodGoal;
+  final String focusArea;
+  final List<String> children;
+
+  Father({
+    required this.name,
+    required this.fatherhoodGoal,
+    required this.focusArea,
+    required this.children,
+  });
+}
