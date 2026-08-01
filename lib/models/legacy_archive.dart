1f2fb7399ce76db44a43be5df02966aa193cc497
lbillbill710 <lbillbill710@gmail.com>
2026-08-01 06:27:31 -0400

    Create legacy_archive.dart

---
+++ lib/models/legacy_archive.dart
@@ -0,0 +1,19 @@
+class LegacyArchive {
+  final String ownerName;
+  final List<String> letters;
+  final List<String> voiceMessages;
+  final List<String> videos;
+  final List<String> photos;
+  final List<String> memories;
+  final List<String> lessons;
+
+  LegacyArchive({
+    required this.ownerName,
+    required this.letters,
+    required this.voiceMessages,
+    required this.videos,
+    required this.photos,
+    required this.memories,
+    required this.lessons,
+  });
+}
