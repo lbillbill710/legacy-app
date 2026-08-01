ddcc08df6ce617b28352b907ebf84a5911d7cfa1
lbillbill710 <lbillbill710@gmail.com>
2026-08-01 06:24:41 -0400

    Create memory.dart

---
+++ lib/models/memory.dart
@@ -0,0 +1,15 @@
+class Memory {
+  final String title;
+  final String description;
+  final String type;
+  final DateTime dateCreated;
+  final String childName;
+
+  Memory({
+    required this.title,
+    required this.description,
+    required this.type,
+    required this.dateCreated,
+    required this.childName,
+  });
+}
